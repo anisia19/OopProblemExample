1264caefb385695ba34144173a4f4675041dba77
anisia19 <anisia.fort19@gmail.com>
2024-12-05 02:00:22 +0200

    Created Hotel and base Room classes
    Added attributes and constructors to classes.

---
+++ PooExample/PooExample/Hotel.cs
@@ -0,0 +1,15 @@
+﻿namespace PooExample;
+
+public class Hotel
+{
+    protected string HotelName { get; set; }
+    protected string HotelAddress { get; set; }
+    protected List<Room> HotelRooms;
+
+    public Hotel(string hotelName, string hotelAddress)
+    {
+        HotelName = hotelName;
+        HotelAddress = hotelAddress;
+        HotelRooms = new List<Room>();
+    }
+}
---
+++ PooExample/PooExample/Room.cs
@@ -0,0 +1,15 @@
+﻿namespace PooExample;
+
+public class Room
+{
+    protected int RoomNumber { get; set; }
+    protected int Capacity { get; set; }
+    protected int Tarrif { get; set; }
+
+    public Room(int roomNumber, int capacity, int tarrif)
+    {
+        RoomNumber = roomNumber;
+        Capacity = capacity;
+        Tarrif = tarrif;
+    }
+}
